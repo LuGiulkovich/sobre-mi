9c4b4dcba31575c6c51caa041b8ffcd3d7dc2269
Luciano Giulkovich <134790339+LuGiulkovich@users.noreply.github.com>
2023-08-20 23:14:04 -0300

    Soluciono error de input vacio

--- practico2/juego.js
+++ practico2/juego.js
@@ -1,5 +1,5 @@
 /** Obtencion del nombre ingresado por el jugador. */
-const nombreJugador = document.getElementById('juego__interfaz-input-nombre').value;
+const nombreJugador = document.getElementById('juego__interfaz-input-nombre');
 
 /** Funcion para reiniciar la partida. */
 function reiniciarPartida() {
@@ -7,12 +7,17 @@ function reiniciarPartida() {
 }
 
 /** Funcion para validar que el nombre ingresado por
- * parte del usuario no esté vacio.
- */
+ * parte del usuario no esté vacio. */
 function campoVacio() {
-    if (nombreJugador === '') {
+    const nombre = nombreJugador.value;
+    console.log('funciona');
+    console.log(nombre);
+
+    if (nombre === '') {
+        console.log('hola')
         document.getElementById('juego__mensaje-vacio').classList.add('juego__mensaje-vacio-activado');
     } else {
+        console.log('cualquiera');
         document.getElementById('juego__mensaje-vacio').classList.remove('juego__mensaje-vacio-activado');
         document.getElementById('juego__mensaje-vacio').classList.add('juego__mensaje-vacio');
     }
